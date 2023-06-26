5d1298ef5741c7f1c2296c1a2fe8f23e68703eb6
ivanpg94 <ivand.2222@gmail.com>
2023-06-26 21:10:06 +0200

    responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <title>Carmen Padilla - @yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="https://kit.fontawesome.com/518cb6af80.js" crossorigin="anonymous"></script>
 
